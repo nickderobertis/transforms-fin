bcc2f900bcf486cb31587657b072e58eb0b3df02
Nick DeRobertis <whoopnip@gmail.com>
2020-02-26 08:44:13 -0500

    Restructure tests and add winsorize transform

--- transforms_fin/__init__.py
+++ transforms_fin/__init__.py
@@ -4,9 +4,11 @@ A set of Transforms meant for financial analysis to be used with the datacode pa
 import datacode as dc
 
 from transforms_fin.portfolio import portfolio_transform
+from transforms_fin.winsorize import winsorize_transform
 
 ALL_TRANSFORMS = [
     portfolio_transform,
+    winsorize_transform,
 ]
 
 dc.register_transforms(ALL_TRANSFORMS)
